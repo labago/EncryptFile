5fedf5e35055d00f4b9d914cd328e6455bd55704
Jonathan Lane <jlane09sjp@gmail.com>
2012-11-10 13:45:35 -0500

    error detection added

--- decrypt.py
+++ decrypt.py
@@ -20,7 +20,7 @@ print 'Decrypting file...'
 
 try:
 	the_file = sys.argv[1]
-except NameError:
+except (RuntimeError, TypeError, NameError, ValueError):
 	print "You must specify a file to encrypt"
 	error = true
 
@@ -29,19 +29,25 @@ parts = the_file.split(".")
 # find the type of file extension
 extension = parts[len(parts)-1]
 
-bytes = []
+file_found = True
 
 # try opening the file, if you can, put all the bytes into a large array
 try:
-	with open(the_file, 'rb') as f:
-		while 1:
-			byte_s = f.read(1)
-			if not byte_s:
-				break
-			byte = byte_s[0]
-			bytes.append(byte)
-except NameError:
+	f = open(the_file, 'rb')
+except Exception:
+	file_found = False
 	print "File with that name was not found"
+
+bytes = []
+
+if file_found:
+	while 1:
+		byte_s = f.read(1)
+		if not byte_s:
+			break
+		byte = byte_s[0]
+		bytes.append(byte)
+	f.close()
 
 # output file with whatever file extension we are dealing with.
 f = open('decrypt_output.'+extension,'wb')
